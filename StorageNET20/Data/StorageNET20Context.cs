fafc68791264a6723f53b7e272e1871c88231311
DZM <d.bjorlingh@gmail.com>
2020-10-08 13:58:49 +0200

    Add project files.

---
+++ StorageNET20/Data/StorageNET20Context.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StorageNET20.Models;
+
+namespace StorageNET20.Data
+{
+    public class StorageNET20Context : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+        public StorageNET20Context (DbContextOptions<StorageNET20Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(
+              new Product
+              {
+                  Id = 1,
+                  Name = "Iphone",
+                  Category = "Mobile",
+                  Count = 156,
+                  Description = "Stupid",
+                  OrderDate = DateTime.Now.AddDays(-6),
+                  Price = 15600,
+                  Shelf = "4B"
+              }, new Product
+              {
+                  Id = 2,
+                  Name = "S10",
+                  Category = "Mobile",
+                  Count = 98,
+                  Description = "Better",
+                  OrderDate = DateTime.Now.AddDays(-23),
+                  Price = 12450,
+                  Shelf = "4C"
+              }, new Product
+              {
+                  Id = 3,
+                  Name = "MacBook",
+                  Category = "Computers",
+                  Count = 23,
+                  Description = "Stupid",
+                  OrderDate = DateTime.Now.AddDays(-5),
+                  Price = 45600,
+                  Shelf = "4C"
+              }, new Product
+              {
+                  Id = 4,
+                  Name = "Suface X",
+                  Category = "Computer",
+                  Count = 32,
+                  Description = "Stupid",
+                  OrderDate = DateTime.Now.AddDays(-40),
+                  Price = 26850,
+                  Shelf = "5C"
+              }, new Product
+              {
+                  Id = 5,
+                  Name = "Pen",
+                  Category = "Office Supplies",
+                  Count = 15651,
+                  Description = "Stupid",
+                  OrderDate = DateTime.Now.AddDays(-6),
+                  Price = 1,
+                  Shelf = "45W"
+              }
+
+              );
+        }
+
+    }
+}
---
+++ StorageNET20/Migrations/20201008115504_Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StorageNET20.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    Category = table.Column<string>(nullable: true),
+                    Shelf = table.Column<string>(nullable: true),
+                    Count = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Category", "Count", "Description", "Name", "OrderDate", "Price", "Shelf" },
+                values: new object[,]
+                {
+                    { 1, "Mobile", 156, "Stupid", "Iphone", new DateTime(2020, 10, 2, 13, 55, 3, 993, DateTimeKind.Local).AddTicks(1902), 15600, "4B" },
+                    { 2, "Mobile", 98, "Better", "S10", new DateTime(2020, 9, 15, 13, 55, 3, 996, DateTimeKind.Local).AddTicks(5499), 12450, "4C" },
+                    { 3, "Computers", 23, "Stupid", "MacBook", new DateTime(2020, 10, 3, 13, 55, 3, 996, DateTimeKind.Local).AddTicks(5563), 45600, "4C" },
+                    { 4, "Computer", 32, "Stupid", "Suface X", new DateTime(2020, 8, 29, 13, 55, 3, 996, DateTimeKind.Local).AddTicks(5722), 26850, "5C" },
+                    { 5, "Office Supplies", 15651, "Stupid", "Pen", new DateTime(2020, 10, 2, 13, 55, 3, 996, DateTimeKind.Local).AddTicks(5725), 1, "45W" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
